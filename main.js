e28c5227a964e50e5de89495172230133046d297
gidskql6671 <gidskql6671@gmail.com>
2022-06-10 23:10:07 +0900

    MAX_OLD_NUMBERS 값 증가
    뭔가 문제가 있어서... 해결될 때까지 임시 방편

--- main.js
+++ main.js
@@ -101,7 +101,7 @@ function main() {
             const regex = new RegExp(regexString);
             nrTags = result.filter(d => d.ref.match(regex));
 
-            const MAX_OLD_NUMBERS = 5; //One or two ref deletes might fail, but if we have lots then there's something wrong!
+            const MAX_OLD_NUMBERS = 30; //One or two ref deletes might fail, but if we have lots then there's something wrong!
             if (nrTags.length > MAX_OLD_NUMBERS) {
                 fail(`ERROR: Too many ${prefix}build-number- refs in repository, found ${nrTags.length}, expected only 1. Check your tags!`);
             }
