aa315d6e22799162196d78be3901071abdafa71d
Onyx Mueller <onyx@onyxmueller.net>
2022-10-26 15:05:47 -0600

    write to new GITHUB_OUTPUT environment files

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ function main() {
         let buildNumber = fs.readFileSync(path);
         console.log(`Build number already generated in earlier jobs, using build number ${buildNumber}...`);
         //Setting the output and a environment variable to new build number...
-        fs.writeFileSync(process.env.GITHUB_ENV, `BUILD_NUMBER=${buildNumber}`);
+        fs.writeFileSync(process.env.GITHUB_OUTPUT, `BUILD_NUMBER=${buildNumber}`);
         console.log(`::set-output name=build_number::${buildNumber}`);
         return;
     }
@@ -130,7 +130,7 @@ function main() {
             
             //Setting the output and a environment variable to new build number...
             //fs.writeFileSync('$GITHUB_ENV', `BUILD_NUMBER=${nextBuildNumber}`);
-            fs.writeFileSync(process.env.GITHUB_ENV, `BUILD_NUMBER=${nextBuildNumber}`);
+            fs.writeFileSync(process.env.GITHUB_OUTPUT, `BUILD_NUMBER=${nextBuildNumber}`);
  
             console.log(`::set-output name=build_number::${nextBuildNumber}`);
             //Save to file so it can be used for next jobs...
